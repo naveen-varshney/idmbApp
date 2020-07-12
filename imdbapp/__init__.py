10ad7754f6c55ed9299405f9da5b9558083002a9
Naveen Varshney <naveen.varshney29@gmail.com>
2020-07-12 17:04:46 +0530

    fix issue for mongodb url

--- imdbapp/__init__.py
+++ imdbapp/__init__.py
@@ -1,11 +1,13 @@
+import os 
 from flask import Flask
 from flask_mongoengine import MongoEngine
 from .api.v1.movies.handler import movie_views
 app = Flask(__name__)
 
+MONGO_DB_HOST = os.getenv("MONGODB_URI") or "localhost"
 app.config['MONGODB_SETTINGS'] = {
     "db": "imdbmovie",
-    "host": "localhost",
+    "host": MONGO_DB_HOST,
 }
 app.register_blueprint(movie_views)
 
@@ -17,6 +19,6 @@ pymongo_db = None
 def get_pymongo_client():
     global pymongo_db
     if pymongo_db is None:
-        pymongo_client = MongoClient(MONGODB_SETTINGS["localhost"])
-        pymongo_db = pymongo_client[MONGODB_SETTINGS["db"]]
+        pymongo_client = MongoClient(MONGO_DB_HOST)
+        pymongo_db = pymongo_client["imdbmovie"]
     return pymongo_db
